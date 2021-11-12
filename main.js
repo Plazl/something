3bc36c6a97682ce696fd31320a2deef6cc71e11a
jhnbg rth <I1234itsplaz@outlook.com>
2021-11-12 11:22:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-
+   const initialize = 1;
+          var x = 500;
+          var y = new Decimal (1e390);
+          var z = 50;
+          
+          if (initialize == 1) {
+            if (interval !== setInterval(update, 1000))
+            var interval = setInterval(update, 1000)
+          }
+          function update() {
+          document.getElementById("sp1").innerHTML = x;
+          document.getElementById("sp2").innerHTML = y;
+          document.getElementById("sp3").innerHTML = z;
+          }
