28ed23e81581d99d09978bccc167c06403cc1eb7
jhnbg rth <I1234itsplaz@outlook.com>
2021-11-12 12:42:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
    const initialize = 1;
-          var x = 500;
-          var y = new Decimal (1e390);
-          var z = 50;
+          var x = new Decimal (500);
+          var y = new Decimal (500);
+          var z = new Decimal(500);
           
           if (initialize == 1) {
-            if (interval !== setInterval(update, 1000))
-            var interval = setInterval(update, 1000)
+            if (interval !== setInterval(update, 1000));
+            var interval = setInterval(update, 1000);
           }
           function update() {
           document.getElementById("sp1").innerHTML = x;
           document.getElementById("sp2").innerHTML = y;
           document.getElementById("sp3").innerHTML = z;
           }
+   
+   function changeX() { 
+   x = x.add(addMult);
+       document.getElementById("sp1").innerHTML = x;
+   }
+
+
+   function changeY() { 
+   y = y.add(addMult);
+       document.getElementById("sp2").innerHTML = y;
+   }
+
+
+   function changeZ() { 
+   Z = Z.add(addMult);
+       document.getElementById("sp3").innerHTML = Z;
+   }
